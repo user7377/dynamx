06597f8942c1ed4955d3a8033344bf09f8f0bfa8
sreeram adithya <76157464+user7377@users.noreply.github.com>
2021-02-11 19:12:41 +0530

    Updates

---
+++ precache-manifest.6b91bfd6c6d76d9b34c065a373f47356.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583c2e965d084923c110a164b215e48a",
+    "url": "/dynamx/index.html"
+  },
+  {
+    "revision": "31785f9d3acaa8d72ce0",
+    "url": "/dynamx/static/css/0.9b2f7aac.chunk.css"
+  },
+  {
+    "revision": "073644582b47ab03dc4a",
+    "url": "/dynamx/static/css/3.db345d7c.chunk.css"
+  },
+  {
+    "revision": "7f5cdd1b36c0fb80129c",
+    "url": "/dynamx/static/css/5.c61ffb77.chunk.css"
+  },
+  {
+    "revision": "6ea61ba6115c19241f40",
+    "url": "/dynamx/static/css/6.bf54a11b.chunk.css"
+  },
+  {
+    "revision": "026dbabcfb3671dd386a",
+    "url": "/dynamx/static/css/main.705dca1f.chunk.css"
+  },
+  {
+    "revision": "31785f9d3acaa8d72ce0",
+    "url": "/dynamx/static/js/0.7d00f5ec.chunk.js"
+  },
+  {
+    "revision": "073644582b47ab03dc4a",
+    "url": "/dynamx/static/js/3.337621ae.chunk.js"
+  },
+  {
+    "revision": "7f795516a054e2522828",
+    "url": "/dynamx/static/js/4.17498053.chunk.js"
+  },
+  {
+    "revision": "63c28f3460550eadf6199955a8ca9c59",
+    "url": "/dynamx/static/js/4.17498053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5cdd1b36c0fb80129c",
+    "url": "/dynamx/static/js/5.e7ce4c02.chunk.js"
+  },
+  {
+    "revision": "94d345d07f28e397ce0d275d9e7db62d",
+    "url": "/dynamx/static/js/5.e7ce4c02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea61ba6115c19241f40",
+    "url": "/dynamx/static/js/6.fc145f35.chunk.js"
+  },
+  {
+    "revision": "5d2cc230450669c115b8",
+    "url": "/dynamx/static/js/7.4fde76bd.chunk.js"
+  },
+  {
+    "revision": "026dbabcfb3671dd386a",
+    "url": "/dynamx/static/js/main.906b3939.chunk.js"
+  },
+  {
+    "revision": "fc11c2fe802d00c4f211",
+    "url": "/dynamx/static/js/runtime-main.75ca929c.js"
+  },
+  {
+    "revision": "c6a92d89aa71027ea621d52bb03ad681",
+    "url": "/dynamx/static/media/bubble-tail-left.c6a92d89.svg"
+  },
+  {
+    "revision": "bcf112461b322d3b888e4eb550e3386a",
+    "url": "/dynamx/static/media/camomile_blurred.bcf11246.jpg"
+  },
+  {
+    "revision": "fb38f7d6fbf79f207edfa158c10f16a7",
+    "url": "/dynamx/static/media/check.fb38f7d6.svg"
+  },
+  {
+    "revision": "8e3f975b0dac0d7ddc9f5ab85f24d15a",
+    "url": "/dynamx/static/media/telegram-logo.8e3f975b.svg"
+  }
+]);
